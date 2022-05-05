6b63148d89208c6cf2b9d1a4dad06821e8cb61f0
daniele.commodaro <daniele.commodaro@studio.unibo.it>
2022-05-05 09:46:41 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prova4'`, () => {
+  it(`should have as title 'BeachManagementPlatform'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prova4');
+    expect(app.title).toEqual('BeachManagementPlatform');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prova4 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('BeachManagementPlatform app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http"
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { BeachComponent } from './beach/beach.component';
+import { WeatherComponent } from './weather/weather.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { BannerComponent } from './banner/banner.component';
+import {ButtonModule} from "primeng/button";
+import {CarouselModule} from "primeng/carousel";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { AgmCoreModule } from '@agm/core';
+
+import config from "src/assets/config";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {SnackbarinterceptorService} from "./services/snackbarinterceptor.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskViewComponent,
+    NavigationBarComponent,
+    BeachComponent,
+    WeatherComponent,
+    BannerComponent
   ],
-  imports: [
-    BrowserModule
+    imports: [
+        BrowserModule,
+        RouterModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FontAwesomeModule,
+        ButtonModule,
+        CarouselModule,
+        NgbModule,
+        FormsModule,
+        AgmCoreModule.forRoot({
+          apiKey: config.AGM_API_KEY
+        }),
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        BrowserAnimationsModule,
+    ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SnackbarinterceptorService,
+      multi: true
+    }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
