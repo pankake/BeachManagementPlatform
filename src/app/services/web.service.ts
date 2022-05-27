4fde79132bf0a80bcbe557da36b53bd4af8c74a1
daniele.commodaro <daniele.commodaro@studio.unibo.it>
2022-05-27 11:38:50 +0200

    postman collection added

--- src/app/services/web.service.ts
+++ src/app/services/web.service.ts
@@ -14,14 +14,6 @@ export class WebService {
     return this.http.get(`${this.ROOT_URL}/${uri}`);
   }
 
-  post(uri: string, payload: Object) {
-    return this.http.post(`${this.ROOT_URL}/${uri}`, payload);
-  }
-
-  patch(uri: string, payload: Object) {
-    return this.http.patch(`${this.ROOT_URL}/${uri}`, payload);
-  }
-
   delete(uri: string) {
     return this.http.delete(`${this.ROOT_URL}/${uri}`);
   }
